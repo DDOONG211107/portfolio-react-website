0d54c4ac37498a3ee463369f0afd01eb6963dd78
KimYoungSun <rladudtjs154@naver.com>
2023-12-04 15:19:43 +0900

    자기소개 작성

--- portfolio-react/src/components/main/MyMain.js
+++ portfolio-react/src/components/main/MyMain.js
@@ -1,15 +1,34 @@
 import classes from "./MyMain.module.css";
 import faceImg from "../../public/face_img.png";
+import introduceStrArr from "../../public/data/desStrArr";
+import { FaCheck } from "react-icons/fa";
 
+const dummyString =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+let i = 0;
 export default function MyMain() {
   return (
     <main className={classes.main}>
       <div className={classes.firstCard}>
         <div className={classes.cardLeft}>
           <img src={faceImg} alt="프로필 사진" className={classes.image} />
-          <div className={classes.mobileCardLeft}>자기소개 설명1</div>
+          {/* <div className={classes.mobileCardLeft}>{dummyString}</div> */}
         </div>
-        <div className={classes.cardRight}>자기소개 설명2</div>
+        <div className={classes.cardRight}>
+          <h3>김영선</h3>
+
+          <ol>
+            {introduceStrArr.map((str) => {
+              return (
+                <li>
+                  <FaCheck />
+                  &nbsp;&nbsp;
+                  {str}
+                </li>
+              );
+            })}
+          </ol>
+        </div>
       </div>
       <div className={classes.secondCard}>
         <div className={classes.cardDes}>마이홈 계산기 설명</div>
---
+++ portfolio-react/src/public/data/desStrArr.js
@@ -0,0 +1,10 @@
+const introduceStrArr = [
+  "인하대학교 컴퓨터공학과 졸업",
+  "javascript, React, ReactNative, Next 프로젝트 개발 경험 보유",
+  "Javascript, python, C++",
+  "vercel 및 aws로 웹사이트 배포 경험 있음",
+  "꼼꼼하고 근성있는 성격",
+  "팀원과 적극적으로 소통하고 프로젝트 스케줄을 세밀하게 관리하고 체크하는 타입",
+];
+
+export default introduceStrArr;
